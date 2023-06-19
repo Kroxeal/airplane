52d6ce11cc03bd2e62f9f231e89e3e6dfe52dd40
Zhenya <Zhenya.zaychenko.2017@mail.ru>
2023-06-19 19:07:21 +0300

    added many to many connection OrdersUsers, adminka changed

--- airplane/booking/admin.py
+++ airplane/booking/admin.py
@@ -1,6 +1,50 @@
 from django.contrib import admin
-
+from .models import *
+from django.utils.html import format_html
+from django.db.models import F
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 
+@admin.register((Airplanes))
+class AirplanesAdmin(admin.ModelAdmin):
+    list_display = ['name', 'seat']
 
+
+# class OrdersAdmin(admin.ModelAdmin):
+#
+
+@admin.register(Passports)
+class PassportsAdmin(admin.ModelAdmin):
+    list_display = ['passport_number', 'nationality', 'sex', 'date_of_birth', 'date_of_issue', 'date_of_expire', 'photo']
+    # fields = ['passport_number', 'nationality', 'sex', 'date_of_birth', 'date_of_issue', 'date_of_expire', 'photo']
+    list_editable = ['nationality', 'sex']
+    list_filter = (
+        ('nationality'),
+    )
+    ordering = ['sex', 'nationality']
+
+
+@admin.register(Routes)
+class RoutesAdmin(admin.ModelAdmin):
+    list_display = ['country_departure', 'city_departure', 'country_arrival', 'city_arrival', 'date_departure', 'date_arrival', 'price']
+    # fields =
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'phone', 'email', 'passport']
+
+@admin.register(Orders)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ['airplane', 'route']
+
+@admin.register(OrdersUsers)
+class OrdersUsersAdmin(admin.ModelAdmin):
+    list_display = ['user', 'order']
+
+# admin.site.register(Airplanes)
+# admin.site.register(Orders)
+# admin.site.register(Users)
+# admin.site.register(Passports)
+# admin.site.register(Routes)
+# admin.site.register(OrdersUsers)
--- airplane/booking/models.py
+++ airplane/booking/models.py
@@ -17,22 +17,27 @@ class Airplanes(models.Model):
 
 
 class Orders(models.Model):
-    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    # user = models.ForeignKey('Users', on_delete=models.CASCADE)
     airplane = models.OneToOneField('Airplanes', on_delete=models.CASCADE)
-    route = models.OneToOneField('Routes', on_delete=models.CASCADE)
-    seat_number = models.PositiveIntegerField(null=True)
-    price = models.PositiveIntegerField()
+    route = models.OneToOneField('Routes', on_delete=models.CASCADE, null=True)
+    # seat_number = models.PositiveIntegerField(null=True)
 
     def __str__(self):
-        return f"{self.user}"
+        return f"{self.airplane}"
+
+
+class OrdersUsers(models.Model):
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    order = models.ForeignKey('Orders', on_delete=models.CASCADE)
 
 
 class Users(models.Model):
     name = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
     phone = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     passport = models.OneToOneField('Passports', on_delete=models.CASCADE)
+    orders_users_mtm = models.ManyToManyField('Orders', through='OrdersUsers')
 
     def __str__(self):
         return f"{self.name}"
@@ -42,9 +47,9 @@ class Passports(models.Model):
     passport_number = models.CharField(max_length=40)
     nationality = models.CharField(max_length=40)
     sex = models.CharField(max_length=1, choices=SEX)
-    date_of_birth = models.DateTimeField(auto_now_add=True)
-    date_of_issue = models.DateTimeField(auto_now_add=True)
-    date_of_expire = models.DateTimeField(auto_now_add=True)
+    date_of_birth = models.DateField()
+    date_of_issue = models.DateField()
+    date_of_expire = models.DateField()
     photo = models.ImageField(upload_to='photo_user', null=True)
 
     def __str__(self):
@@ -52,10 +57,13 @@ class Passports(models.Model):
 
 
 class Routes(models.Model):
-    country_departure = models.CharField(max_length=40)
-    country_arrival = models.CharField(max_length=40)
+    country_departure = models.CharField(max_length=40, default='Belarus')
+    country_arrival = models.CharField(max_length=40, default='Poland')
+    city_departure = models.CharField(max_length=40, default='Minsk')
+    city_arrival = models.CharField(max_length=40, default='Warsaw')
     date_departure = models.DateTimeField(auto_now_add=True)
     date_arrival = models.DateTimeField(auto_now_add=True)
+    price = models.PositiveIntegerField(default=328)
 
     def __str__(self):
-        return f"{self.country_departure}"
+        return f"from {self.country_departure} to {self.country_arrival}"
